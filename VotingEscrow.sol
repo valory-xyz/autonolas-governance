646cf136ca432db703b3a3787d61d6104617cd88
AL <info@77ph.net>
2022-05-26 07:03:26 -0500

    refactor: first gas optimization of veOLA

--- VotingEscrow.sol
+++ VotingEscrow.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./ERC20VotesNonTransferable.sol";
 import "../interfaces/IStructs.sol";
 
@@ -35,13 +31,14 @@ achieved with the longest lock possible. This way the users are incentivized to 
 * What we can do is to extrapolate ***At functions */
 
 struct LockedBalance {
+    // TODO change to uint256
     int128 amount;
     uint256 end;
 }
 
 /// @notice This token supports the ERC20 interface specifications except for transfers.
-contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
-    using SafeERC20 for IERC20;
+contract VotingEscrow is IStructs, ERC20VotesNonTransferable {
+    // using SafeERC20 for IERC20;
 
     enum DepositType {
         DEPOSIT_FOR_TYPE,
@@ -54,10 +51,12 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
     event Withdraw(address indexed provider, uint256 amount, uint256 ts);
     event Supply(uint256 prevSupply, uint256 supply);
 
+    // constants aren't stored in storage anywhere; they're substituted in the bytecode
     // 1 week time
     uint256 internal constant WEEK = 1 weeks;
     // Maximum lock time (4 years)
     uint256 internal constant MAXTIME = 4 * 365 * 86400;
+    // TODO return iMaxTime
 
     // Token address
     address immutable public token;
@@ -76,11 +75,15 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
     mapping(uint256 => int128) public mapSlopeChanges;
 
     // Number of decimals
-    uint8 public decimals;
+    // Fixed to immutable
+    uint8 public immutable decimals;
     // Voting token name
     string public name;
     // Voting token symbol
     string public symbol;
+
+    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol
+    uint256 private locked = 1;
 
     /// @dev Contract constructor
     /// @param _token Token address.
@@ -91,7 +94,9 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         token = _token;
         name = _name;
         symbol = _symbol;
-        decimals = ERC20(_token).decimals();
+        // decimals = ERC20(_token).decimals();
+        // We know this number because we launch the tokens (OLA/veOLA) at the same time and will not change it.
+        decimals = 18;
         // Create initial point such that default timestamp and block number are not zero
         mapSupplyPoints[0] = PointVoting(0, 0, block.timestamp, block.number, 0);
     }
@@ -183,7 +188,10 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
             for (uint256 i = 0; i < 255; ++i) {
                 // Hopefully it won't happen that this won't get used in 5 years!
                 // If it does, users will be able to withdraw but vote weight will be broken
-                tStep += WEEK;
+                // No overflow
+                unchecked {
+                    tStep += WEEK;
+                }
                 int128 dSlope;
                 if (tStep > block.timestamp) {
                     tStep = block.timestamp;
@@ -204,7 +212,10 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
                 lastPoint.ts = tStep;
                 lastPoint.blockNumber = initialPoint.blockNumber + (block_slope * (tStep - initialPoint.ts)) / 1e18;
                 lastPoint.balance = initialPoint.balance;
-                curNumPoint += 1;
+                // No overflow
+                unchecked {
+                    curNumPoint += 1;    
+                }
                 if (tStep == block.timestamp) {
                     lastPoint.blockNumber = block.number;
                     lastPoint.balance = supply;
@@ -275,12 +286,17 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         DepositType depositType
     ) internal {
         uint256 supplyBefore = supply;
-        supply = supplyBefore + amount;
+        // Cannot overflow because supply << 2^255
+        unchecked {
+            supply = supplyBefore + amount;
+        }
         // Get the old locked data
         LockedBalance memory oldLocked;
         (oldLocked.amount, oldLocked.end) = (lockedBalance.amount, lockedBalance.end);
         // Adding to existing lock, or if a lock is expired - creating a new one
-        lockedBalance.amount += int128(int256(amount));
+        unchecked {
+            lockedBalance.amount += int128(int256(amount));
+        }
         if (unlockTime != 0) {
             lockedBalance.end = unlockTime;
         }
@@ -291,10 +307,13 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         // amount == 0 (extend lock) or amount > 0 (add to lock or extend lock)
         // lockedBalance.end > block.timestamp (always)
         _checkpoint(account, oldLocked, lockedBalance);
-
-        address from = msg.sender;
-        if (amount != 0) {
-            IERC20(token).safeTransferFrom(from, address(this), amount);
+        if (amount > 0) {
+            // OLA is full standard token 
+            // with correct function transfer(address to, uint256 amount) public virtual returns (bool)
+            // we can avoid https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol#L29
+            // TODO add check of success
+            bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+            // TODO revert if fail
         }
 
         emit Deposit(account, amount, lockedBalance.end, depositType, block.timestamp);
@@ -311,27 +330,39 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
     ///      cannot extend their locktime and deposit for a brand new user.
     /// @param account Account address.
     /// @param amount Amount to add.
-    function depositFor(address account, uint256 amount) external nonReentrant {
+    function depositFor(address account, uint256 amount) external {
+        if(locked > 1) {
+            revert ReentrancyGuard();
+        }
+        locked = 2;
         LockedBalance memory lockedBalance = mapLockedBalances[account];
         // Check if the amount is zero
+        // not optimized by logic
         if (amount == 0) {
             revert ZeroValue();
         }
         // The locked balance must already exist
+        // not optimized by logic
         if (lockedBalance.amount == 0) {
             revert NoValueLocked(account);
         }
         // Check the lock expiry
-        if (lockedBalance.end <= block.timestamp) {
+        // probably optimized
+        if (lockedBalance.end < (block.timestamp +1)) {
             revert LockExpired(msg.sender, lockedBalance.end, block.timestamp);
         }
         _depositFor(account, amount, 0, lockedBalance, DepositType.DEPOSIT_FOR_TYPE);
+        locked = 1;
     }
 
     /// @dev Deposits `amount` tokens for `msg.sender` and lock until `unlockTime`.
     /// @param amount Amount to deposit.
     /// @param unlockTime Time when tokens unlock, rounded down to a whole week.
-    function createLock(uint256 amount, uint256 unlockTime) external nonReentrant {
+    function createLock(uint256 amount, uint256 unlockTime) external  {
+        if(locked > 1) {
+            revert ReentrancyGuard();
+        }
+        locked = 2;
         // Lock time is rounded down to weeks
         unlockTime = ((block.timestamp + unlockTime) / WEEK) * WEEK;
         LockedBalance memory lockedBalance = mapLockedBalances[msg.sender];
@@ -353,11 +384,16 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         }
 
         _depositFor(msg.sender, amount, unlockTime, lockedBalance, DepositType.CREATE_LOCK_TYPE);
+        locked = 1;
     }
 
     /// @dev Deposits `amount` additional tokens for `msg.sender` without modifying the unlock time.
     /// @param amount Amount of tokens to deposit and add to the lock.
-    function increaseAmount(uint256 amount) external nonReentrant {
+    function increaseAmount(uint256 amount) external {
+        if(locked > 1) {
+            revert ReentrancyGuard();
+        }
+        locked = 2;
         LockedBalance memory lockedBalance = mapLockedBalances[msg.sender];
         // Check if the amount is zero
         if (amount == 0) {
@@ -368,28 +404,35 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
             revert NoValueLocked(msg.sender);
         }
         // Check the lock expiry
-        if (lockedBalance.end <= block.timestamp) {
+        if (lockedBalance.end < (block.timestamp + 1)) {
             revert LockExpired(msg.sender, lockedBalance.end, block.timestamp);
         }
 
         _depositFor(msg.sender, amount, 0, lockedBalance, DepositType.INCREASE_LOCK_AMOUNT);
+        locked = 1;
     }
 
     /// @dev Extends the unlock time.
     /// @param unlockTime New tokens unlock time.
-    function increaseUnlockTime(uint256 unlockTime) external nonReentrant {
+    function increaseUnlockTime(uint256 unlockTime) external {
+        if(locked > 1) {
+            revert ReentrancyGuard();
+        }
+        locked = 2;
         LockedBalance memory lockedBalance = mapLockedBalances[msg.sender];
+        // The TIMESTAMP op code costs 2 gas, according to the yellow paper
+        // optimal as is
         unlockTime = ((block.timestamp + unlockTime) / WEEK) * WEEK;
         // The locked balance must already exist
         if (lockedBalance.amount == 0) {
             revert NoValueLocked(msg.sender);
         }
         // Check the lock expiry
-        if (lockedBalance.end <= block.timestamp) {
+        if (lockedBalance.end < (block.timestamp + 1)) {
             revert LockExpired(msg.sender, lockedBalance.end, block.timestamp);
         }
         // Check for the lock time correctness
-        if (unlockTime <= lockedBalance.end) {
+        if (unlockTime < (lockedBalance.end + 1)) {
             revert UnlockTimeIncorrect(msg.sender, lockedBalance.end, unlockTime);
         }
         // Check for the lock time not to exceed the MAXTIME
@@ -398,10 +441,15 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         }
 
         _depositFor(msg.sender, 0, unlockTime, lockedBalance, DepositType.INCREASE_UNLOCK_TIME);
+        locked = 1;
     }
 
     /// @dev Withdraws all tokens for `msg.sender`. Only possible if the lock has expired.
-    function withdraw() external nonReentrant {
+    function withdraw() external {
+        if(locked > 1) {
+            revert ReentrancyGuard();
+        }
+        locked = 2;
         LockedBalance memory lockedBalance = mapLockedBalances[msg.sender];
         if (lockedBalance.end > block.timestamp) {
             revert LockNotExpired(msg.sender, lockedBalance.end, block.timestamp);
@@ -410,17 +458,20 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
 
         mapLockedBalances[msg.sender] = LockedBalance(0,0);
         uint256 supplyBefore = supply;
-        supply = supplyBefore - amount;
-
+        unchecked {
+            supply = supplyBefore - amount;
+        }
         // oldLocked can have either expired <= timestamp or zero end
         // lockedBalance has only 0 end
         // Both can have >= 0 amount
         _checkpoint(msg.sender, lockedBalance, LockedBalance(0,0));
 
         emit Withdraw(msg.sender, amount, block.timestamp);
-        emit Supply(supplyBefore, supply);
+        emit Supply(supplyBefore, (supplyBefore - amount));
 
-        IERC20(token).safeTransfer(msg.sender, amount);
+        // IERC20(token).safeTransfer(msg.sender, amount);
+        IERC20(token).transfer(msg.sender, amount);
+        locked = 1;
     }
 
     /// @dev Finds a closest point that has a specified block number.
@@ -438,7 +489,9 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
             if (maxPointNumber == 0) {
                 return (point, minPointNumber);
             }
-            maxPointNumber -= 1;
+            unchecked {
+                maxPointNumber -= 1;
+            }
         } else {
             maxPointNumber = totalNumPoints;
         }
@@ -465,6 +518,7 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         }
 
         // Get the found point
+        // TODO change != to ==
         if (account != address(0)) {
             point = mapUserPoints[account][minPointNumber];
         } else {
@@ -478,6 +532,7 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
     /// @return vBalance Account voting power.
     function _balanceOfLocked(address account, uint256 ts) internal view returns (uint256 vBalance) {
         uint256 pointNumber = mapUserPoints[account].length;
+        // TODO change to pointNumber > 0
         if (pointNumber == 0) {
             return 0;
         } else {
@@ -511,7 +566,7 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         // Find point with the closest block number to the provided one
         (PointVoting memory uPoint, ) = _findPointByBlock(blockNumber, account);
         // If the block number at the point index is bigger than the specified block number, the balance was zero
-        if (uPoint.blockNumber <= blockNumber) {
+        if (uPoint.blockNumber < (blockNumber + 1)) {
             balance = uPoint.balance;
         }
     }
@@ -576,7 +631,9 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
     function _supplyLockedAt(PointVoting memory lastPoint, uint256 ts) internal view returns (uint256 vSupply) {
         uint256 tStep = (lastPoint.ts / WEEK) * WEEK;
         for (uint256 i = 0; i < 255; ++i) {
-            tStep += WEEK;
+            unchecked {
+                tStep += WEEK;
+            }
             int128 dSlope;
             if (tStep > ts) {
                 tStep = ts;
@@ -609,7 +666,7 @@ contract VotingEscrow is IStructs, ReentrancyGuard, ERC20VotesNonTransferable {
         // Find point with the closest block number to the provided one
         (PointVoting memory sPoint, ) = _findPointByBlock(blockNumber, address(0));
         // If the block number at the point index is bigger than the specified block number, the balance was zero
-        if (sPoint.blockNumber <= blockNumber) {
+        if (sPoint.blockNumber < (blockNumber + 1)) {
             supplyAt = sPoint.balance;
         }
     }
