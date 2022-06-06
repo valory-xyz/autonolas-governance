831b4e4d171668eeff1ad7d0cee7f15780b8b06b
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-06-06 22:18:13 +0100

    feat: porting OLA token from its own repository

--- contracts/test/TokenFactory.sol
+++ contracts/test/TokenFactory.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-// Getting ABIs for the OLA token related routines
-import "../../lib/autonolas-token/src/OLA.sol";
 // Getting ABIs for Gnosis Safe related routines
 import "@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol";
 import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
