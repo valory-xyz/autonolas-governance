dac87a297f11353cdc4d5ba493fc9aec493a69e2
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-06-08 13:37:02 +0100

    feat and test: sale contract refinement and test coverage

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -15,10 +15,19 @@ interface ILOCK {
     function createLockFor(address account, uint256 amount, uint256 unlockTime) external;
 }
 
-// Struct for storing balance, lock and unlock time
+// Interface for the OLAS token allowance increase
+interface IOLAS {
+    /// @dev Increases the allowance of another account over their tokens.
+    /// @param spender Account that tokens are approved for.
+    /// @param amount Amount to increase approval by.
+    /// @return True if the operation succeeded.
+    function increaseAllowance(address spender, uint256 amount) external returns (bool);
+}
+
+// Struct for storing claimable balance, lock and unlock time
 // The struct size is one storage slot of uint256 (128 + 64 + padding)
-struct LockedBalance {
-    // Token amount locked. Initial OLAS cap is 1 bn tokens, or 1e27.
+struct ClaimableBalance {
+    // Token amount to be locked. Initial OLAS cap is 1 bn tokens, or 1e27.
     // After 10 years, the inflation rate is 2% per year. It would take 1340+ years to reach 2^128 - 1 total supply
     uint128 amount;
     // Lock time period or number of steps
@@ -28,34 +37,40 @@ struct LockedBalance {
 
 /// @notice This token supports the ERC20 interface specifications except for transfers.
 contract Sale is IErrors {
-    event LockVE(address indexed account, uint256 amount, uint256 timePeriod);
-    event LockBU(address indexed account, uint256 amount, uint256 numSteps);
+    event CreateVE(address indexed account, uint256 amount, uint256 timePeriod);
+    event CreateBU(address indexed account, uint256 amount, uint256 numSteps);
     event ClaimVE(address indexed account, uint256 amount, uint256 timePeriod);
     event ClaimBU(address indexed account, uint256 amount, uint256 numSteps);
     event OwnerUpdated(address indexed owner);
 
     // Maximum number of steps for buOLAS (synced with buOLAS `MAX_NUM_STEPS`)
     uint256 internal constant MAX_NUM_STEPS = 10;
+    // Minimum lock time for veOLAS (1 year)
+    uint256 internal constant MINTIME = 365 * 86400;
     // Maximum lock time for veOLAS (synced with veOLAS `MAXTIME`)
     uint256 internal constant MAXTIME = 4 * 365 * 86400;
     // Reentrancy lock
     uint256 private locked = 1;
+    // OLAS token address
+    address public immutable olasToken;
     // veOLAS token address
     address public immutable veToken;
     // buOLAS token address
     address public immutable buToken;
     // Owner address
     address public owner;
-    // Mapping of account address => LockedBalance to lock for veOLAS
-    mapping(address => LockedBalance) public mapVE;
-    // Mapping of account address => LockedBalance to lock for buOLAS
-    mapping(address => LockedBalance) public mapBU;
+    // Mapping of account address => ClaimableBalance to lock for veOLAS
+    mapping(address => ClaimableBalance) public mapVE;
+    // Mapping of account address => ClaimableBalance to lock for buOLAS
+    mapping(address => ClaimableBalance) public mapBU;
 
     /// @dev Contract constructor
+    /// @param _olasToken OLAS token address.
     /// @param _veToken veOLAS token address.
     /// @param _buToken buOLAS token address.
-    constructor(address _veToken, address _buToken)
+    constructor(address _olasToken, address _veToken, address _buToken)
     {
+        olasToken = _olasToken;
         veToken = _veToken;
         buToken = _buToken;
         owner = msg.sender;
@@ -83,7 +98,7 @@ contract Sale is IErrors {
     /// @param buAccounts Accounts for buOLAS locks.
     /// @param buAmounts Amounts for `buAccounts`.
     /// @param buNumSteps Lock time for `buAccounts`.
-    function createLocksFor(
+    function createBalancesFor(
         address[] memory veAccounts,
         uint256[] memory veAmounts,
         uint256[] memory veLockTimes,
@@ -97,12 +112,15 @@ contract Sale is IErrors {
         }
 
         // Check that all the corresponding arrays have the same length
-        if (veAccounts.length != veAmounts.length && veAccounts.length != veLockTimes.length) {
+        if (veAccounts.length != veAmounts.length || veAccounts.length != veLockTimes.length) {
             revert WrongArrayLength(veAccounts.length, veAmounts.length);
         }
-        if (buAccounts.length != buAmounts.length && buAccounts.length != buNumSteps.length) {
+        if (buAccounts.length != buAmounts.length || buAccounts.length != buNumSteps.length) {
             revert WrongArrayLength(buAccounts.length, buAmounts.length);
         }
+
+        uint256 allowanceVE;
+        uint256 allowanceBU;
 
         // Create lock-ready structures for veOLAS
         for (uint256 i = 0; i < veAccounts.length; ++i) {
@@ -110,26 +128,34 @@ contract Sale is IErrors {
             if (veAccounts[i] == address(0)) {
                 revert ZeroAddress();
             }
-            // Check the end of a lock time
-            if (veLockTimes[i] > MAXTIME) {
-                revert Overflow(veLockTimes[i], MAXTIME);
+            // Check for other zero values
+            if (veAmounts[i] == 0) {
+                revert ZeroValue();
             }
             // Check for the amount bounds
             if (veAmounts[i] > type(uint128).max) {
                 revert Overflow(veAmounts[i], type(uint128).max);
             }
+            // Check the end of a lock time
+            if (veLockTimes[i] < MINTIME) {
+                revert UnlockTimeIncorrect(veAccounts[i], MINTIME, veLockTimes[i]);
+            }
+            if (veLockTimes[i] > MAXTIME) {
+                revert MaxUnlockTimeReached(veAccounts[i], MAXTIME, veLockTimes[i]);
+            }
             // Check if the lock has already been placed
-            LockedBalance memory lockedBalance = mapVE[veAccounts[i]];
+            ClaimableBalance memory lockedBalance = mapVE[veAccounts[i]];
             if (lockedBalance.amount > 0) {
                 revert NonZeroValue();
             }
 
-            // Push values to the dedicated locking slot
+            // Update allowance, push values to the dedicated locking slot
+            allowanceVE += veAmounts[i];
             lockedBalance.amount = uint128(veAmounts[i]);
             lockedBalance.period = uint64(veLockTimes[i]);
             mapVE[veAccounts[i]] = lockedBalance;
 
-            emit LockVE(veAccounts[i], veAmounts[i], veLockTimes[i]);
+            emit CreateVE(veAccounts[i], veAmounts[i], veLockTimes[i]);
         }
 
         // Create lock-ready structures for buOLAS
@@ -138,26 +164,39 @@ contract Sale is IErrors {
             if (buAccounts[i] == address(0)) {
                 revert ZeroAddress();
             }
-            // Check for the number of lock steps
-            if (buNumSteps[i] > MAX_NUM_STEPS) {
-                revert Overflow(buNumSteps[i], MAX_NUM_STEPS);
+            // Check for other zero values
+            if (buAmounts[i] == 0 || buNumSteps[i] == 0) {
+                revert ZeroValue();
             }
             // Check for the amount bounds
             if (buAmounts[i] > type(uint128).max) {
                 revert Overflow(buAmounts[i], type(uint128).max);
             }
+            // Check for the number of lock steps
+            if (buNumSteps[i] > MAX_NUM_STEPS) {
+                revert Overflow(buNumSteps[i], MAX_NUM_STEPS);
+            }
             // Check if the lock has already been placed
-            LockedBalance memory lockedBalance = mapBU[buAccounts[i]];
+            ClaimableBalance memory lockedBalance = mapBU[buAccounts[i]];
             if (lockedBalance.amount > 0) {
                 revert NonZeroValue();
             }
 
-            // Push values to the dedicated locking slot
+            // Update allowance, push values to the dedicated locking slot
+            allowanceBU += buAmounts[i];
             lockedBalance.amount = uint128(buAmounts[i]);
             lockedBalance.period = uint64(buNumSteps[i]);
             mapBU[buAccounts[i]] = lockedBalance;
 
-            emit LockBU(buAccounts[i], buAmounts[i], buNumSteps[i]);
+            emit CreateBU(buAccounts[i], buAmounts[i], buNumSteps[i]);
+        }
+
+        // Increase allowances
+        if (allowanceVE > 0) {
+            IOLAS(olasToken).increaseAllowance(address(veToken), allowanceVE);
+        }
+        if (allowanceBU > 0) {
+            IOLAS(olasToken).increaseAllowance(address(buToken), allowanceBU);
         }
     }
 
@@ -170,20 +209,40 @@ contract Sale is IErrors {
         locked = 2;
 
         // Get the balance, lock time and call the veOLAS locking function
-        LockedBalance memory lockedBalance = mapVE[msg.sender];
+        ClaimableBalance memory lockedBalance = mapVE[msg.sender];
         if (lockedBalance.amount > 0) {
             ILOCK(veToken).createLockFor(msg.sender, uint256(lockedBalance.amount), uint256(lockedBalance.period));
-            mapVE[msg.sender] = LockedBalance(0, 0);
+            mapVE[msg.sender] = ClaimableBalance(0, 0);
             emit ClaimVE(msg.sender, uint256(lockedBalance.amount), uint256(lockedBalance.period));
         }
 
         lockedBalance = mapBU[msg.sender];
         if (lockedBalance.amount > 0) {
             ILOCK(buToken).createLockFor(msg.sender, uint256(lockedBalance.amount), uint256(lockedBalance.period));
-            mapBU[msg.sender] = LockedBalance(0, 0);
+            mapBU[msg.sender] = ClaimableBalance(0, 0);
             emit ClaimBU(msg.sender, uint256(lockedBalance.amount), uint256(lockedBalance.period));
         }
 
         locked = 1;
+    }
+
+    /// @dev Gets the veOLAS claim status.
+    /// @param account Account address.
+    /// @return status True if the balance is not zero.
+    function isClaimableVE(address account) external view returns (bool status) {
+        uint256 balance = uint256(mapVE[account].amount);
+        if (balance > 0) {
+            status = true;
+        }
+    }
+
+    /// @dev Gets the buOLAS claim status.
+    /// @param account Account address.
+    /// @return status True if the balance is not zero.
+    function isClaimableBU(address account) external view returns (bool status) {
+        uint256 balance = uint256(mapBU[account].amount);
+        if (balance > 0) {
+            status = true;
+        }
     }
 }
---
+++ test/Sale.js
@@ -0,0 +1,301 @@
+/*global describe, context, beforeEach, it*/
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Sale contract", function () {
+    let olas;
+    let ve;
+    let bu;
+    let sale;
+    let signers;
+    const initialMint = "1000000000000000000000000"; // 1000000
+    const oneWeek = 7 * 86400;
+    const oneYear = 365 * 86400;
+    const numSteps = 4;
+    const oneOLABalance = ethers.utils.parseEther("1");
+    const twoOLABalance = ethers.utils.parseEther("2");
+    const AddressZero = "0x" + "0".repeat(40);
+    const overflowNum128 = "341" + "0".repeat(36);
+
+    beforeEach(async function () {
+        const OLAS = await ethers.getContractFactory("OLAS");
+        olas = await OLAS.deploy(0);
+        await olas.deployed();
+
+        signers = await ethers.getSigners();
+        await olas.mint(signers[0].address, initialMint);
+
+        const VE = await ethers.getContractFactory("veOLAS");
+        ve = await VE.deploy(olas.address, "name", "symbol");
+        await ve.deployed();
+
+        const BU = await ethers.getContractFactory("buOLAS");
+        bu = await BU.deploy(olas.address, "name", "symbol");
+        await bu.deployed();
+
+        const SALE = await ethers.getContractFactory("Sale");
+        sale = await SALE.deploy(olas.address, ve.address, bu.address);
+        await sale.deployed();
+    });
+
+    context("Create balances for account", async function () {
+        it("Changing owner", async function () {
+            const owner = signers[0];
+            const account = signers[1];
+
+            // Trying to change owner from a non-owner account address
+            await expect(
+                sale.connect(account).changeOwner(account.address)
+            ).to.be.revertedWith("OwnerOnly");
+
+            // Trying to change owner for the zero address
+            await expect(
+                sale.connect(owner).changeOwner(AddressZero)
+            ).to.be.revertedWith("ZeroAddress");
+
+            // Changing the owner
+            await sale.connect(owner).changeOwner(account.address);
+
+            // Trying to change owner from the previous owner address
+            await expect(
+                sale.connect(owner).changeOwner(owner.address)
+            ).to.be.revertedWith("OwnerOnly");
+        });
+
+        it("Should fail when creating a claimable balance with wrong arguments", async function () {
+            const account = signers[1].address;
+
+            // Trying to call the createBalancesFor from a non owner
+            await expect(
+                sale.connect(signers[1]).createBalancesFor([], [], [], [], [], [])
+            ).to.be.revertedWith("OwnerOnly");
+
+            // **************************   veOLAS   *************************
+            // Address for veOLAS is zero
+            await expect(
+                sale.createBalancesFor([AddressZero], [0], [0], [], [], [])
+            ).to.be.revertedWith("ZeroAddress");
+
+            // Amount for veOLAS is zero
+            await expect(
+                sale.createBalancesFor([account], [0], [0], [], [], [])
+            ).to.be.revertedWith("ZeroValue");
+
+            // Amount for veOLAS is above the limit
+            await expect(
+                sale.createBalancesFor([account], [overflowNum128], [oneYear], [], [], [])
+            ).to.be.revertedWith("Overflow");
+
+            // Time for veOLAS is less than one year
+            await expect(
+                sale.createBalancesFor([account], [oneOLABalance], [0], [], [], [])
+            ).to.be.revertedWith("UnlockTimeIncorrect");
+
+            // Time for veOLAS is bigger than the max time of 4 years
+            await expect(
+                sale.createBalancesFor([account], [oneOLABalance], [4 * oneYear + 1], [], [], [])
+            ).to.be.revertedWith("MaxUnlockTimeReached");
+
+            // Number array arguments for veOLAS is incorrect
+            await expect(
+                sale.createBalancesFor([account], [oneOLABalance, twoOLABalance], [oneYear], [], [], [])
+            ).to.be.revertedWith("WrongArrayLength");
+
+            await expect(
+                sale.createBalancesFor([account], [oneOLABalance], [oneYear, 2 * oneYear], [], [], [])
+            ).to.be.revertedWith("WrongArrayLength");
+
+            // **************************   buOLAS   *************************
+            // Address for buOLAS is zero
+            await expect(
+                sale.createBalancesFor([], [], [], [AddressZero], [0], [0])
+            ).to.be.revertedWith("ZeroAddress");
+
+            // Amount for buOLAS is zero
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [0], [0])
+            ).to.be.revertedWith("ZeroValue");
+
+            // Amount for buOLAS is above the limit
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [overflowNum128], [1])
+            ).to.be.revertedWith("Overflow");
+
+            // Number of time steps for buOLAS is zero
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [oneOLABalance], [0])
+            ).to.be.revertedWith("ZeroValue");
+
+            // Number of time steps for buOLAS is more than the maximum number of steps
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [oneOLABalance], [11])
+            ).to.be.revertedWith("Overflow");
+
+            // Number array arguments for buOLAS is incorrect
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [oneOLABalance, twoOLABalance], [1])
+            ).to.be.revertedWith("WrongArrayLength");
+
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [oneOLABalance], [1, 2])
+            ).to.be.revertedWith("WrongArrayLength");
+        });
+
+        it("Create balances for", async function () {
+            const account = signers[1].address;
+
+            // Check the account possibility to claim
+            let status = await sale.isClaimableVE(account);
+            expect(status).to.equal(false);
+
+            status = await sale.isClaimableBU(account);
+            expect(status).to.equal(false);
+
+            // Create balances for veOLAS and buOLAS and check statuses
+            sale.createBalancesFor([account], [oneOLABalance], [oneYear], [account], [oneOLABalance], [numSteps]);
+
+            status = await sale.isClaimableVE(account);
+            expect(status).to.equal(true);
+
+            status = await sale.isClaimableBU(account);
+            expect(status).to.equal(true);
+
+            // Try to create additional balance after the existent one
+            await expect(
+                sale.createBalancesFor([account], [oneOLABalance], [oneYear], [], [], [])
+            ).to.be.revertedWith("NonZeroValue");
+
+            await expect(
+                sale.createBalancesFor([], [], [], [account], [oneOLABalance], [numSteps])
+            ).to.be.revertedWith("NonZeroValue");
+        });
+    });
+
+    context("Claim", async function () {
+        it("Claim for one account with veOLAS", async function () {
+            const account = signers[1];
+
+            // Mint OLAS for the Sale contract
+            olas.mint(sale.address, oneOLABalance);
+
+            // Create balance for account in veOLAS
+            await sale.createBalancesFor([account.address], [oneOLABalance], [oneYear], [], [], []);
+
+            // Claim the balance for veOLAS
+            await sale.connect(account).claim();
+            // Record the current block (needed for checking the unlock time)
+            const block = await ethers.provider.getBlock("latest");
+
+            // Trying to claim more will not do anything
+            let status = await sale.isClaimableVE(account.address);
+            expect(status).to.equal(false);
+            await sale.connect(account).claim();
+
+            // Check the status of the lock in veOLAS
+            // Check the balance
+            const balance = await ve.balanceOf(account.address);
+            expect(balance).to.equal(oneOLABalance);
+            // Check the lock time
+            const lockEnd = await ve.lockedEnd(account.address);
+            expect(Math.floor((block.timestamp + oneYear) / oneWeek) * oneWeek).to.equal(lockEnd);
+        });
+
+        it("Claim for one account with buOLAS", async function () {
+            const account = signers[1];
+
+            // Mint OLAS for the Sale contract
+            olas.mint(sale.address, oneOLABalance);
+
+            // Create balance for account in buOLAS
+            await sale.createBalancesFor([], [], [], [account.address], [oneOLABalance], [numSteps]);
+
+            // Claim the balance for buOLAS
+            await sale.connect(account).claim();
+            // Record the current block (needed for checking the unlock time)
+            const block = await ethers.provider.getBlock("latest");
+
+            // Trying to claim more will not do anything
+            let status = await sale.isClaimableBU(account.address);
+            expect(status).to.equal(false);
+            await sale.connect(account).claim();
+
+            // Check the status of the lock in buOLAS
+            // Check the balance
+            const balance = await bu.balanceOf(account.address);
+            expect(balance).to.equal(oneOLABalance);
+            // Check the lock time
+            const lockEnd = await bu.lockedEnd(account.address);
+            expect(block.timestamp + oneYear * numSteps).to.equal(lockEnd);
+        });
+
+        it("Claim for one account with both veOLAS and buOLAS", async function () {
+            const account = signers[1];
+
+            // Mint OLAS for the Sale contract
+            olas.mint(sale.address, twoOLABalance);
+
+            // Create balance for account in veOLAS and buOLAS
+            await sale.createBalancesFor([account.address], [oneOLABalance], [oneYear],
+                [account.address], [oneOLABalance], [numSteps]);
+
+            // Claim the balance for both veOLAS and buOLAS
+            await sale.connect(account).claim();
+            // Record the current block (needed for checking the unlock time)
+            const block = await ethers.provider.getBlock("latest");
+
+            // Check the claim status for both tokens
+            let status = await sale.isClaimableVE(account.address);
+            expect(status).to.equal(false);
+            status = await sale.isClaimableBU(account.address);
+            expect(status).to.equal(false);
+
+            // Check the status of the locks
+            // Check the balance
+            let balance = await ve.balanceOf(account.address);
+            expect(balance).to.equal(oneOLABalance);
+            balance = await bu.balanceOf(account.address);
+            expect(balance).to.equal(oneOLABalance);
+            // Check the lock time
+            let lockEnd = await ve.lockedEnd(account.address);
+            expect(Math.floor((block.timestamp + oneYear) / oneWeek) * oneWeek).to.equal(lockEnd);
+            lockEnd = await bu.lockedEnd(account.address);
+            expect(block.timestamp + oneYear * numSteps).to.equal(lockEnd);
+        });
+
+        it("Claim for two accounts with veOLAS", async function () {
+            const accounts = [signers[1], signers[2]];
+
+            // Mint OLAS for the Sale contract
+            olas.mint(sale.address, twoOLABalance);
+
+            // Create balance for account in veOLAS
+            await sale.createBalancesFor([accounts[0].address, accounts[1].address], [oneOLABalance, oneOLABalance],
+                [oneYear, oneYear], [], [], []);
+
+            // Claim the balance for veOLAS
+            await sale.connect(accounts[0]).claim();
+            await sale.connect(accounts[1]).claim();
+            // Record the current block (needed for checking the unlock time)
+            const block = await ethers.provider.getBlock("latest");
+
+            // Check the claim status for both tokens
+            let status = await sale.isClaimableVE(accounts[0].address);
+            expect(status).to.equal(false);
+            status = await sale.isClaimableVE(accounts[1].address);
+            expect(status).to.equal(false);
+
+            // Check the status of the locks
+            // Check the balance
+            let balance = await ve.balanceOf(accounts[0].address);
+            expect(balance).to.equal(oneOLABalance);
+            balance = await ve.balanceOf(accounts[1].address);
+            expect(balance).to.equal(oneOLABalance);
+            // Check the lock time
+            let lockEnd = await ve.lockedEnd(accounts[0].address);
+            expect(Math.floor((block.timestamp + oneYear) / oneWeek) * oneWeek).to.equal(lockEnd);
+            lockEnd = await ve.lockedEnd(accounts[1].address);
+            expect(Math.floor((block.timestamp + oneYear) / oneWeek) * oneWeek).to.equal(lockEnd);
+        });
+    });
+});
