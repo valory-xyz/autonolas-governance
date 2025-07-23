4b724a89de663ec17e7f19e20698997b5a651bcf
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-07-23 16:06:31 +0100

    chore: sloc for unaudited code

--- contracts/multisigs/test/MockTreasury.sol
+++ contracts/multisigs/test/MockTreasury.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.30;
 
 /// @dev Only `owner` has a privilege, but the `sender` was provided.
 /// @param sender Sender address.
